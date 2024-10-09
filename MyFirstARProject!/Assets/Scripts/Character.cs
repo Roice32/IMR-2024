842619e9a747fd93dc9e3571fa64f1ec419d60d5
Roice32 <alexandru.butnaru32@yahoo.com>
2024-10-09 20:25:53 +0300

    Fixed script for both characters to interact as expected
    But the Attack animations keep moving the models. Needs fixing.

--- MyFirstARProject!/Assets/Scripts/Character.cs
+++ MyFirstARProject!/Assets/Scripts/Character.cs
@@ -1,18 +1,27 @@
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    private Animator animationController;
+    private Animator AnimationController;
+    private List<Character> OtherCharacters;
 
     void Start()
     {
-        animationController = GetComponent<Animator>();
+        GetAnimationController();
+        DetectOtherCharacters();
+        SignalMySpawningToOthers();
+    }
 
-        if (animationController != null)
+    private void GetAnimationController()
+    {
+        AnimationController = GetComponent<Animator>();
+
+        if (AnimationController != null)
         {
-            animationController.SetBool("Idling", true);
+            AnimationController.SetBool("Idling", true);
         }
         else
         {
@@ -20,9 +29,43 @@ public class Character : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
+    private void DetectOtherCharacters()
+    {
+        OtherCharacters = FindObjectsOfType<Character>().ToList();
+        OtherCharacters.Remove(this);
+    }
+
+    private void SignalMySpawningToOthers()
+    {
+        foreach (Character character in OtherCharacters)
+        {
+            character.RegisterNewCharacterInScene(this);
+        }
+    }
+
+    public void RegisterNewCharacterInScene(Character newCharacter)
+    {
+        OtherCharacters.Add(newCharacter);
+    }
+
     void Update()
     {
+        Vector3 myPosition = transform.position;
         
+        foreach (Character otherCharacter in OtherCharacters)
+        {
+            Vector3 otherPosition = otherCharacter.transform.position;
+            float distance = Vector3.Distance(myPosition, otherPosition);
+            
+            if (distance <= 0.1f)
+            {
+                AnimationController.SetBool("Attacking", true);
+                AnimationController.SetBool("Idling", false);
+                return;
+            }
+        }
+
+        AnimationController.SetBool("Idling", true);
+        AnimationController.SetBool("Attacking", false);
     }
 }
