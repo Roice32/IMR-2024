d39802f559ef6546c99c3f311d8e0027cb378645
Roice32 <alexandru.butnaru32@yahoo.com>
2024-10-15 12:56:12 +0300

    Added minimal VFX when hitting the ball

--- VRMinigolf/Assets/Scripts/BallBehavior.cs
+++ VRMinigolf/Assets/Scripts/BallBehavior.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections;
 using UnityEngine;
 
 public class BallBehavior : MonoBehaviour
@@ -8,9 +8,14 @@ public class BallBehavior : MonoBehaviour
     private static readonly Vector3 RESET_VELOCITY = new(0, 0, 0);
     private static readonly Vector3 RESET_COORDS = new(0, 0.25f, 0);
     private static readonly int HIT_COOLDOWN_MILLIS = 1000;
-    
+
+    private static readonly int HIT_VFX_DURATION_MILLIS = 500;
+    private static readonly Vector3 INITIAL_VFX_SIZE = new(0.01f, 0.01f, 0.01f);
+    private static readonly Vector3 FINAL_VFX_SIZE = new(0.3f, 0.3f, 0.3f);
+
     private GameLogic GameLogic;
     private Rigidbody ControlledRigidBody;
+    public GameObject HitVFXPrefab;
 
     public int TimesHit { get; private set; }
     private int CooldownMillisLeft;
@@ -50,12 +55,39 @@ public class BallBehavior : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Club"))
         {
-            if (CooldownMillisLeft == 0)
-            {
-                CooldownMillisLeft = HIT_COOLDOWN_MILLIS;
-                TimesHit++;
-                GameLogic.UpdateOnBallHit();
-            }
+            HandleHitByClub(collision);
         }
     }
+
+    private void HandleHitByClub(Collision collision)
+    {
+        if (CooldownMillisLeft > 0)
+        {
+            return;
+        }
+        
+        CooldownMillisLeft = HIT_COOLDOWN_MILLIS;
+        TimesHit++;
+        GameLogic.UpdateOnBallHit();
+        TriggerHitVFX(collision.contacts[0].point);
+    }
+
+    private void TriggerHitVFX(Vector3 coords)
+    {
+        GameObject hitVFXInstance = Instantiate(HitVFXPrefab, coords, Quaternion.identity);
+        StartCoroutine(HitVFXAnimation(hitVFXInstance));
+    }
+
+    private IEnumerator HitVFXAnimation(GameObject hitVFXInstance)
+    {
+        float timePassed = 0;
+        while (timePassed < HIT_VFX_DURATION_MILLIS)
+        {
+            hitVFXInstance.transform.localScale = Vector3.Lerp(INITIAL_VFX_SIZE, FINAL_VFX_SIZE, timePassed / HIT_VFX_DURATION_MILLIS);
+            timePassed += Time.deltaTime * 1000;
+            yield return null;
+        }
+        Destroy(hitVFXInstance);
+    }
+
 }
